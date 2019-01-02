fff2316ff45390d9832bf2138188e95a4bd18b3c
alex <>
2019-01-02 16:29:15 -0500

    install package

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <ZoomableScatterplot/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+import ZoomableScatterplot from "./components/ZoomableScatterplot.vue";
 
 export default {
   name: "app",
   components: {
-    // HelloWorld
+    // HelloWorld,
+    ZoomableScatterplot
   }
 };
 </script>
